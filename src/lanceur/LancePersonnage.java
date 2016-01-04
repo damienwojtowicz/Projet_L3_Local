9d4978ac0db67189a8bda1b5ae35777637532e48
Nemesis31 <monster.co31@gmail.com>
2016-01-04 19:15:23 +0100

    Update LancePersonnage.java
    permet de lancer 5 personnages en 1 clic.

--- src/lanceur/LancePersonnage.java
+++ src/lanceur/LancePersonnage.java
@@ -20,70 +20,71 @@ public class LancePersonnage {
 	private static String usage = "USAGE : java " + LancePersonnage.class.getName() + " [ port [ ipArene ] ]";
 
 	public static void main(String[] args) {
-		String nom = "Truc";
+		String[] nom = {"Damien", "Thib", "Guilhem", "Hassen", "Hodor"};
 		
-		// TODO remplacer la ligne suivante par votre numero de groupe
-		String groupe = "G" + Calculs.nombreAleatoire(0,99); 
-		
-		// nombre de tours pour ce personnage avant d'etre deconnecte 
-		// (30 minutes par defaut)
-		// si negatif, illimite
-		int nbTours = Constantes.NB_TOURS_PERSONNAGE_DEFAUT;
-		
-		// init des arguments
-		int port = Constantes.PORT_DEFAUT;
-		String ipArene = Constantes.IP_DEFAUT;
-		
-		if (args.length > 0) {
-			if (args[0].equals("--help") || args[0].equals("-h")) {
-				ErreurLancement.aide(usage);
+		for(int i = 0 ; i<nom.length ; i++){
+			String groupe = "G1.2";
+			
+			// nombre de tours pour ce personnage avant d'etre deconnecte 
+			// (30 minutes par defaut)
+			// si negatif, illimite
+			int nbTours = Constantes.NB_TOURS_PERSONNAGE_DEFAUT;
+			
+			// init des arguments
+			int port = Constantes.PORT_DEFAUT;
+			String ipArene = Constantes.IP_DEFAUT;
+			
+			if (args.length > 0) {
+				if (args[0].equals("--help") || args[0].equals("-h")) {
+					ErreurLancement.aide(usage);
+				}
+				
+				if (args.length > 2) {
+					ErreurLancement.TROP_ARGS.erreur(usage);
+				}
+				
+				try {
+					port = Integer.parseInt(args[0]);
+				} catch (NumberFormatException e) {
+					ErreurLancement.PORT_NAN.erreur(usage);
+				}
+				
+				if (args.length > 1) {
+					ipArene = args[1];
+				}
 			}
 			
-			if (args.length > 2) {
-				ErreurLancement.TROP_ARGS.erreur(usage);
-			}
-			
+			// creation du logger
+			LoggerProjet logger = null;
 			try {
-				port = Integer.parseInt(args[0]);
-			} catch (NumberFormatException e) {
-				ErreurLancement.PORT_NAN.erreur(usage);
+				logger = new LoggerProjet(true, "personnage_" + nom[i] + groupe);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(ErreurLancement.suivant);
 			}
 			
-			if (args.length > 1) {
-				ipArene = args[1];
+			// lancement du serveur
+			try {
+				String ipConsole = InetAddress.getLocalHost().getHostAddress();
+				
+				logger.info("Lanceur", "Creation du personnage...");
+				
+				// caracteristiques du personnage
+				HashMap<Caracteristique, Integer> caracts = new HashMap<Caracteristique, Integer>();
+				// seule la force n'a pas sa valeur par defaut (exemple)
+				caracts.put(Caracteristique.FORCE, 
+						Calculs.valeurCaracAleatoire(Caracteristique.FORCE)); 
+				
+				Point position = Calculs.positionAleatoireArene();
+				
+				new StrategiePersonnage(ipArene, port, ipConsole, nom[i], groupe, caracts, nbTours, position, logger);
+				logger.info("Lanceur", "Creation du personnage reussie");
+				
+			} catch (Exception e) {
+				logger.severe("Lanceur", "Erreur lancement :\n" + e.getCause());
+				e.printStackTrace();
+				System.exit(ErreurLancement.suivant);
 			}
-		}
-		
-		// creation du logger
-		LoggerProjet logger = null;
-		try {
-			logger = new LoggerProjet(true, "personnage_" + nom + groupe);
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(ErreurLancement.suivant);
-		}
-		
-		// lancement du serveur
-		try {
-			String ipConsole = InetAddress.getLocalHost().getHostAddress();
-			
-			logger.info("Lanceur", "Creation du personnage...");
-			
-			// caracteristiques du personnage
-			HashMap<Caracteristique, Integer> caracts = new HashMap<Caracteristique, Integer>();
-			// seule la force n'a pas sa valeur par defaut (exemple)
-			caracts.put(Caracteristique.FORCE, 
-					Calculs.valeurCaracAleatoire(Caracteristique.FORCE)); 
-			
-			Point position = Calculs.positionAleatoireArene();
-			
-			new StrategiePersonnage(ipArene, port, ipConsole, nom, groupe, caracts, nbTours, position, logger);
-			logger.info("Lanceur", "Creation du personnage reussie");
-			
-		} catch (Exception e) {
-			logger.severe("Lanceur", "Erreur lancement :\n" + e.getCause());
-			e.printStackTrace();
-			System.exit(ErreurLancement.suivant);
 		}
 	}
 }
